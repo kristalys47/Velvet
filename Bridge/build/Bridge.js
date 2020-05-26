fbbcd77c77da255feb1524dd0e6912ec7a9e88c0
Nunila <nunila.davila@upr.edu>
2020-05-26 02:24:04 -0400

    before merge

--- Bridge/build/Bridge.js
+++ Bridge/build/Bridge.js
@@ -1,11 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const H1_1 = require("./HTML-Objects/H/H1");
-const H2_1 = require("./HTML-Objects/H/H2");
-const H3_1 = require("./HTML-Objects/H/H3");
-const H4_1 = require("./HTML-Objects/H/H4");
-const H5_1 = require("./HTML-Objects/H/H5");
-const H6_1 = require("./HTML-Objects/H/H6");
+exports.templatePath = exports.Bridge = void 0;
+const HTag_1 = require("./HTML-Objects/HTag");
 const P_1 = require("./HTML-Objects/P");
 const IMG_1 = require("./HTML-Objects/IMG");
 const HTMLObjects_1 = require("./HTML-Objects/ENUM/HTMLObjects");
@@ -18,28 +14,8 @@ class Bridge {
         // Crea los objetos con la info del JSON.
         inputData.forEach((instaceData) => {
             switch (instaceData.type) {
-                case HTMLObjects_1.HTMLObjects.H1: {
-                    this.HTMLObjects.push(new H1_1.H1(instaceData.id));
-                    break;
-                }
-                case HTMLObjects_1.HTMLObjects.H2: {
-                    this.HTMLObjects.push(new H2_1.H2(instaceData.id));
-                    break;
-                }
-                case HTMLObjects_1.HTMLObjects.H3: {
-                    this.HTMLObjects.push(new H3_1.H3(instaceData.id));
-                    break;
-                }
-                case HTMLObjects_1.HTMLObjects.H4: {
-                    this.HTMLObjects.push(new H4_1.H4(instaceData.id));
-                    break;
-                }
-                case HTMLObjects_1.HTMLObjects.H5: {
-                    this.HTMLObjects.push(new H5_1.H5(instaceData.id));
-                    break;
-                }
-                case HTMLObjects_1.HTMLObjects.H6: {
-                    this.HTMLObjects.push(new H6_1.H6(instaceData.id));
+                case HTMLObjects_1.HTMLObjects.HTag: {
+                    this.HTMLObjects.push(new HTag_1.HTag(instaceData.id));
                     break;
                 }
                 case HTMLObjects_1.HTMLObjects.P: {
--- Bridge/build/HTML-Objects/MainHTMLObject/HTMLObject.js
+++ Bridge/build/HTML-Objects/MainHTMLObject/HTMLObject.js
@@ -9,24 +9,28 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.HTMLObject = void 0;
 const inversify_1 = require("inversify");
 const types_1 = require("../../types");
 const DOM_service_1 = require("../../Services/DOM.service");
 const di_container_1 = require("../../di-container");
-class HTMLObject {
-    constructor(id, type) {
-        this.id = '';
-        this.style = '';
-        this.domService = di_container_1.DIContainer.resolve(DOM_service_1.DOMService);
-        this.id = id;
-        this.type = type;
+let HTMLObject = /** @class */ (() => {
+    class HTMLObject {
+        constructor(id, type) {
+            this.id = '';
+            this.style = '';
+            this.domService = di_container_1.DIContainer.resolve(DOM_service_1.DOMService);
+            this.id = id;
+            this.type = type;
+        }
+        getId() { return this.id; }
+        getType() { return this.type; }
+        getStyle() { return this.style; }
     }
-    getId() { return this.id; }
-    getType() { return this.type; }
-    getStyle() { return this.style; }
-}
-__decorate([
-    inversify_1.inject(types_1.TYPES.IHTag),
-    __metadata("design:type", DOM_service_1.DOMService)
-], HTMLObject.prototype, "domService", void 0);
+    __decorate([
+        inversify_1.inject(types_1.TYPES.IHTag),
+        __metadata("design:type", DOM_service_1.DOMService)
+    ], HTMLObject.prototype, "domService", void 0);
+    return HTMLObject;
+})();
 exports.HTMLObject = HTMLObject;
